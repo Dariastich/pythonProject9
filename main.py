d25fa7a348f1885419fea99d5c71e2e8cbdfc581
Dariastich <94920518+Dariastich@users.noreply.github.com>
2022-02-22 19:47:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask_wtf import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('base.html', title= title)
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
